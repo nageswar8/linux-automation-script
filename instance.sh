e211cf13359912d38dac1ad22d1bfe5db53eb2b7
nageswar8 <nageswarreddy447@gmail.com>
2025-09-23 23:25:06 +0530

    shell script

--- instance.sh
+++ instance.sh
@@ -1,1 +1,34 @@
 echo "Hello, I came here to automate"
+
+
+DOMAIN_NAME="idiap.shop"
+
+for i in $@:
+do 
+    INSTANCE_ID=$(aws ec2 run-instances  --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0fecc4fefc0ae3b31 --region us-east-1 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$i'}]' --query 'Instances[0].InstanceId' --output text)
+    if [ i -ne "frontend"]; then 
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].PrivateIpAddress' --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].PublicIpAddress' --output text)
+    fi
+
+        aws route53 change-resource-record-sets  --hosted-zone-id Z09590552TMQIL7M10P1I \
+        --change-batch '{
+        "Comment": "Creating an A record",
+        "Changes": [
+        {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$i.$DOMAIN_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                    {
+                        "Value": "'$IP'"
+                    }
+                ]
+            }
+        }
+    ]
+}'
+done
