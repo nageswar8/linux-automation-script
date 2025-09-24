b702db762643fc8308060aaabd0006dcd172532f
nageswar8 <nageswarreddy447@gmail.com>
2025-09-24 23:16:30 +0530

    shell script

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -23,7 +23,7 @@ fi
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-mkdir  $LOGS_FOLDER
+mkdir -p $LOGS_FOLDER
 
 echo "Script started executed $(date)" | tee -a $LOG_FILE
 
