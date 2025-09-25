e2c55ed8153bee9b8a83085eb8849bfeffa6c138
nageswar8 <nageswarreddy447@gmail.com>
2025-09-25 08:09:00 +0530

    shell script

--- redis.sh
+++ redis.sh
@@ -53,7 +53,7 @@ else
 fi
 
 
-sed -i "/s/127.0.0.1/0.0.0.0/g" -e "c protected-mode/protected-mode no" /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e 's/^protected-mode yes/protected-mode no/' /etc/redis/redis.conf
 
 systemctl enable redis 
 VALIDATE $? "enabling "
