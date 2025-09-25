8e815144b9919dc46a57347a281e7ce54deadb3b
nageswar8 <nageswarreddy447@gmail.com>
2025-09-25 07:59:24 +0530

    shell script

---
+++ redis.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+EX_ST=$( date +%s )
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R error:: user need root privileges"
+    exit 1 
+fi
+
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executed $(date)" | tee -a $LOG_FILE
+
+VALIDATE() {
+
+    if [ $1 -ne 0 ]; then
+        echo -e "Installing $2 ... $R FAILED $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$G Installing $2 ...  SUCCESS $N" | tee -a $LOG_FILE
+    fi
+
+}
+
+
+dnf module disable redis -y
+
+VALIDATE $? "disable redis"
+dnf module enable redis:7 -y
+
+VALIDATE $? "enable redis"
+
+dnf list installed redis &>>$LOG_FILE
+
+
+if [ $? -ne 0 ]; then
+    dnf install redis -y &>>$LOG_FILE
+    VALIDATE $? "redis"
+else
+    echo -e "redis already exist ... $Y SKIPPING $N"
+fi
+
+
+sed -i "/s/127.0.0.1/0.0.0.0/g" -e "c protected-mode/protected-mode no" /etc/redis/redis.conf
+
+systemctl enable redis 
+VALIDATE $? "enabling "
+systemctl start redis 
+VALIDATE $? "starting "
+
+ED_TM=$( date +%s)
+
+DU=$(($ED_TM - $EX_ST))
+echo -e "Execution time $Y $DU secodns $N"
---
+++ user.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+# MONGO_HOST=mongodb.idiap.shop
+CUR_DIR=$PWD
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R error:: user need root privileges"
+    exit 1 
+fi
+
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executed $(date)" | tee -a $LOG_FILE
+
+VALIDATE() {
+
+    if [ $1 -ne 0 ]; then
+        echo -e "Installing $2 ... $R FAILED $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$G Installing $2 ...  SUCCESS $N" | tee -a $LOG_FILE
+    fi
+
+}
+
+dnf module list nodejs &>>$LOG_FILE
+
+VALIDATE $? "getting the node list"
+
+dnf module disable nodejs -y &>>$LOG_FILE
+
+VALIDATE $? "Disable nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "enable 20 nodejs"
+
+dnf install nodejs -y &>>$LOG_FILE
+
+VALIDATE $? "installing nodejs"
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "creating user"
+else
+    echo "Roboshop user already created"
+fi
+
+mkdir -p /app
+
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+VALIDATE $? "downloading user"
+cd /app 
+
+rm -rf /app/* &>>$LOG_FILE
+VALIDATE $? " Remove old code "
+unzip /tmp/user.zip &>>$LOG_FILE
+VALIDATE $? "unzip user"
+
+npm install &>>$LOG_FILE
+
+VALIDATE $? "install dependencies"
+
+cp $CUR_DIR/user.service /etc/systemd/system/user.service
+VALIDATE $? "copying service"
+
+systemctl daemon-reload
+VALIDATE $? "reloading daemon"
+
+systemctl enable user &>>$LOG_FILE 
+VALIDATE $? "enable user"
+systemctl start user &>>$LOG_FILE
+VALIDATE $? "start user"
